32c989f83a89b9aabc0386d304e99192b9332c1f
sk <lylapp@163.com>
2018-08-21 15:04:00 +0800

    add

---
+++ SKKit/Sources/Category/View/UIView+alert.h
@@ -0,0 +1,13 @@
+//
+//  UIView+alert.h
+//  SKKit
+//
+//  Created by sk on 2018/8/21.
+//  Copyright © 2018 sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (alert)
+
+@end
---
+++ SKKit/Sources/Category/View/UIView+alert.m
@@ -0,0 +1,13 @@
+//
+//  UIView+alert.m
+//  SKKit
+//
+//  Created by sk on 2018/8/21.
+//  Copyright © 2018 sk. All rights reserved.
+//
+
+#import "UIView+alert.h"
+#import "UIView+viewController.h"
+@implementation UIView (alert)
+
+@end
